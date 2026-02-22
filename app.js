67853b96e0f7d4e9cc5e13a729841d40f33b43e2
AlmondTeaa <almontejosc@gmail.com>
2026-02-22 09:55:12 +0800

    "Server: done"

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const path = require('path');
 
 const app = express();
 
+const fetchGetApi  = async (url) => {
+    try {
+        const apiResponse = await axios.get(url);
+        return apiResponse.data;
+    }
+    catch (error) {
+        console.log(`API error: ${error.response?.data?.message || error.message}`);
+        throw new Error(`API error: ${error.response?.data?.message || error.message}`);
+    }
+}
 //Set template
 app.set('view engine', 'ejs');
 //Serve public folder for CSS
@@ -14,16 +24,44 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded());
 
 
-app.get("/", (req,res,next) => {
+app.get("/", async (req,res,next) => {
     res.render("index",{weather:null, error:null})
 })
 
-
-app.get("/weather", (req,res,next) => {
+ 
+app.get("/weather",async (req,res,next) => {
+    const apiKey = `baf6389b06620e9626487c5dbc695395`;
     const city = req.query.location;
-    res.send(`Searching for weather in: ${city}`);
+    const apiURLGeo = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`;
+
+    let error = null;
+    let lat, lon;
+    let weather;
+
+    try {
+        const geoResponse = await fetchGetApi(apiURLGeo);
+        const [location] = geoResponse;
+        lat = location.lat;
+        lon = location.lon;
+
+    } catch (apiError) {
+        error = apiError;
+    }
+
+    let apiURLWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+
+    try {
+        const weatherResponse = await fetchGetApi(apiURLWeather);
+        weather = weatherResponse.weather;
+
+    } catch (apiError) {
+        error = apiError;
+    }
+    
+    res.render("index", {weather, error});
 })
 
 
 console.log("Server Listening to Port 3000");
 app.listen(3000);
+
